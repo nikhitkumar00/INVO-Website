ec2a0c2907ed50947a70ed6152d0b940f705981b
Nikhit Kumar <nikhitkumar00@gmail.com>
2023-12-30 00:55:00 +0530

    shadow fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,7 +54,7 @@ function Navbar() {
                         <path d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z"></path>
                     </svg>
                     {isOpen ? (
-                        <div className="w-46 absolute z-20 right-0 top-10 flex flex-col gap-3 rounded-md bg-gray-200 p-1 shadow-lg shadow-gray-400 md:hidden dark:bg-slate-800 dark:text-white">
+                        <div className="w-46 absolute z-20 right-0 top-10 flex flex-col gap-3 rounded-md bg-gray-200 p-1 shadow-sm shadow-gray-400 md:hidden dark:bg-slate-800 dark:text-white">
                             <a
                                 className="flex gap-2 rounded-md px-4 py-2 text-sm hover:bg-gray-300 dark:hover:bg-slate-700"
                                 href="#"
