d23b6a5117984a5c37af01dc7f2456c694776560
Nikhit Kumar <nikhitkumar00@gmail.com>
2023-12-29 22:51:20 +0530

    changes to file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Body from "./Components/Body";
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
