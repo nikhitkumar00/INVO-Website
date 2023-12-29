055fdccc45da66dea381b6908722a22cf3e762ef
Nikhit Kumar <nikhitkumar00@gmail.com>
2023-12-30 00:02:46 +0530

    added darkmode svg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,18 @@
 function Footer() {
     return (
-        <div className="mt-3 hidden animate-bounce justify-center sm:flex">
+        <div className="flex animate-bounce justify-center">
             <svg
-                className="w-16"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 76 76"
+                className="w-7"
+                viewBox="0 0 15 15"
                 fill="none"
+                xmlns="http://www.w3.org/2000/svg"
             >
                 <path
-                    d="M60.1663 34.8335L37.9997 53.8335L15.833 34.8335"
-                    stroke="#666666"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                />
-                <path
-                    d="M60.1663 22.1667L37.9997 41.1667L15.833 22.1667"
-                    stroke="#666666"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                />
+                    d="M4.18179 6.18181C4.35753 6.00608 4.64245 6.00608 4.81819 6.18181L7.49999 8.86362L10.1818 6.18181C10.3575 6.00608 10.6424 6.00608 10.8182 6.18181C10.9939 6.35755 10.9939 6.64247 10.8182 6.81821L7.81819 9.81821C7.73379 9.9026 7.61934 9.95001 7.49999 9.95001C7.38064 9.95001 7.26618 9.9026 7.18179 9.81821L4.18179 6.81821C4.00605 6.64247 4.00605 6.35755 4.18179 6.18181Z"
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                ></path>
             </svg>
         </div>
     );
