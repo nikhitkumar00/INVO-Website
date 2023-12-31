80b37d4195c0fa5cb904beb118cd2a4fd4454660
Nikhit Kumar <nikhitkumar00@gmail.com>
2023-12-31 13:41:04 +0530

    cleaned the animation

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,8 @@
 function Body() {
     return (
-        <div className="relative flex h-[calc(100vh-64px)] flex-col items-center justify-between px-10 pb-16 md:flex-row  dark:bg-gray-950  dark:text-white">
-            <div className="p-12 w-1/2">
-                <img src="../../images/pc.webp" alt="computer" loading="lazy"/>
+        <div className="relative flex h-[calc(100vh-64px)] flex-col items-center justify-start px-10 md:flex-row  dark:bg-gray-950  dark:text-white">
+            <div className="w-full p-12 md:w-1/2">
+                <img src="../../images/pc.webp" alt="computer" loading="lazy" />
             </div>
             <div className="flex w-full flex-col justify-center gap-5 md:gap-7">
                 <div>
@@ -46,15 +46,15 @@ function Body() {
                         Documentation
                     </button>
                 </div>
-            </div>
-            <div className="absolute bottom-1 flex animate-bounce left-0 w-full justify-center">
-                <svg
-                    className="w-10 rounded-full bg-gray-200 fill-black p-2"
-                    viewBox="0 0 15 15"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path d="M4.18179 6.18181C4.35753 6.00608 4.64245 6.00608 4.81819 6.18181L7.49999 8.86362L10.1818 6.18181C10.3575 6.00608 10.6424 6.00608 10.8182 6.18181C10.9939 6.35755 10.9939 6.64247 10.8182 6.81821L7.81819 9.81821C7.73379 9.9026 7.61934 9.95001 7.49999 9.95001C7.38064 9.95001 7.26618 9.9026 7.18179 9.81821L4.18179 6.81821C4.00605 6.64247 4.00605 6.35755 4.18179 6.18181Z"></path>
-                </svg>
+                <div className="flex justify-center items-center">
+                    <svg
+                        className="w-10 animate-bounce rounded-full bg-gray-200 fill-black p-2"
+                        viewBox="0 0 15 15"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M4.18179 6.18181C4.35753 6.00608 4.64245 6.00608 4.81819 6.18181L7.49999 8.86362L10.1818 6.18181C10.3575 6.00608 10.6424 6.00608 10.8182 6.18181C10.9939 6.35755 10.9939 6.64247 10.8182 6.81821L7.81819 9.81821C7.73379 9.9026 7.61934 9.95001 7.49999 9.95001C7.38064 9.95001 7.26618 9.9026 7.18179 9.81821L4.18179 6.81821C4.00605 6.64247 4.00605 6.35755 4.18179 6.18181Z"></path>
+                    </svg>
+                </div>
             </div>
         </div>
     );
